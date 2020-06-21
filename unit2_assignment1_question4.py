c3fa419e9c6d3dc7b87ffb0eb60e9bb50c19cc0a
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-21 22:38:23 +0530

    Add files via upload

---
+++ unit2_assignment1_question4.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enter the marks32\n",
+      "student got fail marks\n"
+     ]
+    }
+   ],
+   "source": [
+    "marks=input(\"enter the marks\")\n",
+    "if int(marks)>35:\n",
+    "    print(\"the student got pass marks\")\n",
+    "else:\n",
+    "    print(\"student got fail marks\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
