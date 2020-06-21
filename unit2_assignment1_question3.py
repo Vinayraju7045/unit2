aa105e44394165a8097871587993a5d53d7e62b5
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-21 22:06:42 +0530

    Add files via upload

---
+++ unit2_assignment1_question3.py
@@ -0,0 +1,67 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enter the value of a:2\n",
+      "enter the value of b:3\n",
+      "the sum of and b is equal to 5\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=input(\"enter the value of a:\")\n",
+    "b=input(\"enter the value of b:\")\n",
+    "sum=int(a)+int(b)\n",
+    "if sum==5:\n",
+    "    print(\"the sum of and b is equal to 5\")\n",
+    "elif sum>5:\n",
+    "    print(\"the sum of a and b is grater than 5\")\n",
+    "else:\n",
+    "    print(\"the sum of a and b is less than 5\")\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
