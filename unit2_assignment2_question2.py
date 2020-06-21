3528db00248f977cadeceafc89a2c25083e3f5ab
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-21 21:41:24 +0530

    Add files via upload

---
+++ unit2_assignment2_question2.py
@@ -0,0 +1,68 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enter the value of a5\n",
+      "enter the value of b5\n",
+      "enter the value of c5\n",
+      "all three are equal\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=input(\"enter the value of a\")\n",
+    "b=input(\"enter the value of b\")\n",
+    "c=input(\"enter the value of c\")\n",
+    "if a==b and b==c and c==a:\n",
+    "    print(\"all three are equal\")\n",
+    "elif (a==b and b==a) or (b==c and c==b) or (c==a and a==c):\n",
+    "    print(\"two inputs are equal among the three\")\n",
+    "else:\n",
+    "    print(\"all three are not equal\")\n",
+    "    "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
